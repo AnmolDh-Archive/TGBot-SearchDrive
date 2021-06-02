e5fe4033b9bffd17e99ca7271f6968cf2bc19d7d
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-02 14:23:41 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -1,26 +1,27 @@
 from telegram.ext import CommandHandler, run_async
-from bot.helper.drive_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot import LOGGER, dispatcher
-from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage
 from bot.helper.telegram_helper.filters import CustomFilters
+import threading
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
 @run_async
 def list_drive(update,context):
     try:
         search = update.message.text.split(' ',maxsplit=1)[1]
+        LOGGER.info(f"Searching: {search}")
+        reply = sendMessage('Searching..... Please wait!', context.bot, update)
+        gdrive = GoogleDriveHelper(None)
+        msg, button = gdrive.drive_list(search)
+
+        if button:
+            editMessage(msg, reply, button)
+        else:
+            editMessage('No result found', reply, button)
+
     except IndexError:
-        sendMessage('send a search key along with command', context.bot, update)
-        return
-        
-    reply = sendMessage('Searching...', context.bot, update)
-
-    LOGGER.info(f"Searching: {search}")
-        
-    gdrive = GoogleDriveHelper(None)
-    msg, button = gdrive.drive_list(search)
-
-    editMessage(msg,reply,button)
+        sendMessage('Send a search key along with command', context.bot, update)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
