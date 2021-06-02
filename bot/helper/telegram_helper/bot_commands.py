35aa0b2b3eddde79da0fdbd19ef48235d5b0ff79
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-02 15:48:29 +0530

    Add files via upload

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,10 +1,10 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.ListCommand = 'list'
-        self.AuthorizedUsersCommand = 'users'
-        self.AuthorizeCommand = 'auth'
-        self.UnAuthorizeCommand = 'unauth'
-        self.LogCommand = 'log'
+        self.ListCommand = 'search'
+        self.AuthorizedUsersCommand = 'susers'
+        self.AuthorizeCommand = 'sauth'
+        self.UnAuthorizeCommand = 'sunauth'
+        self.LogCommand = 'slog'
 
 BotCommands = _BotCommands()
